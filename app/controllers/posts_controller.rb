dbae9408ca481074585b1fcb3a505e06885b8b7d
Jaroslav Hanak <HanakJarek@gmail.com>
2015-12-01 13:19:17 +0100

    Fix controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,7 @@ class PostsController < ApplicationController
   def create
     #render plain: params[:post].inspect
     @post = Post.new(params[:post])
+    @post.save
+    redirect_to @post
   end
 end
